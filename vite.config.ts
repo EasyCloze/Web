2e1c70b43964c0b25b8e8f17816bcfaca8bdd79d
Chenqi <1002652154@qq.com>
2023-05-11 18:32:55 +0800

    add service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        'name': 'Easy Cloze',
+        'short_name': 'Easy Cloze',
+        'start_url': '.',
+        'display': 'standalone',
+        'theme_color': '#b0c4de',
+        'background_color': '#b0c4de',
+        'icons': [
+          {
+            'sizes': '192x192',
+            'src': 'android-chrome-192x192.png',
+            'type': 'image/png'
+          },
+          {
+            'sizes': '512x512',
+            'src': 'android-chrome-512x512.png',
+            'type': 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      },
+    })
+  ],
   base: '',
 })
