e7a3658101bc49e18fb66d6bf2de1864a645d74b
Chenqi <1002652154@qq.com>
2023-05-11 10:02:05 +0800

    fix visual viewport

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import { useLocalState } from './utility/localState';
 import { useRefGetSet } from './utility/refGetSet';
-import { useForceUpdate } from './utility/forceUpdate';
-import PositionFixed from './widget/PositionFixed';
 import LanguageProvider from './lang/Provider';
 import Menu from './Menu';
 import List from './List';
 import './App.css';
 
 export default function App() {
+  const ref = useRef();
   const [token, setToken] = useLocalState('token');
   const [getMenuRef, setMenuRef] = useRefGetSet();
   const [getListRef, setListRef] = useRefGetSet();
 
-  const Frame = ({ children }) => {
-    const forceUpdate = useForceUpdate();
-
-    useEffect(() => {
-      window.addEventListener('resize', forceUpdate);
-    }, []);
-
-    return <PositionFixed className='app' zIndex='0' left='0px' top='0px' right='0px' height={window.visualViewport.height}>{children}</PositionFixed>
-  }
+  useEffect(() => {
+    function updatePosition() {
+      ref.current.style.left = visualViewport.offsetLeft + 'px';
+      ref.current.style.top = visualViewport.offsetTop + 'px';
+      ref.current.style.width = visualViewport.width + 'px';
+      ref.current.style.height = visualViewport.height + 'px';
+    }
+    visualViewport.addEventListener('resize', updatePosition);
+    visualViewport.addEventListener('scroll', updatePosition);
+  }, []);
 
   return (
     <LanguageProvider>
-      <Frame>
+      <div ref={ref} className='app'>
         <Menu token={token} setToken={setToken} setMenuRef={setMenuRef} getListRef={getListRef} />
         <List token={token} setToken={setToken} getMenuRef={getMenuRef} setListRef={setListRef} />
-      </Frame>
+      </div>
     </LanguageProvider>
   )
 }
