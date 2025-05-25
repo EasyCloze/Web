6a70ff3e09f949df7ffac988a29e165962eff770
Chenqi <1002652154@qq.com>
2025-05-25 21:41:24 +0200

    fix compile error

--- src/utility/refObj.ts
+++ src/utility/refObj.ts
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 
 export function useRefObj(initializer: () => object): object {
-  const ref : { current : object | undefined } = useRef();
+  const ref : { current : object | undefined } = useRef(undefined);
   if (!ref.current) {
     ref.current = initializer();
   }
