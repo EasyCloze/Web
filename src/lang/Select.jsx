a567008d819c9b5216941e40bf4d73296eeb054a
Chenqi <1002652154@qq.com>
2025-09-19 23:07:25 +0200

    update language select

--- src/lang/Select.jsx
+++ src/lang/Select.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import Placeholder from '../widget/Placeholder';
+import Label from '../widget/Label';
 import Text from './Text';
 import LanguageContext from './context';
 import dict from './dict';
@@ -9,10 +9,9 @@ export default function () {
 
   return (
     <div>
-      <Text id='language_select.text' />
-      <Placeholder width='5px' />
-      <select style={{ height: '32px', padding: '0px 16px' }} value={lang || ''} onChange={event => setLang(event.target.value ? event.target.value : null)}>
-        <option value={''}><Text id='language_select.default.text' /></option>
+      <Label><Text id='dialog.setting.language.text' /></Label>
+      <select style={{ height: '32px', padding: '0px 16px', borderWidth: '1px' }} value={lang || ''} onChange={event => setLang(event.target.value ? event.target.value : null)}>
+        <option value={''}><Text id='dialog.setting.language.default.text' /></option>
         {
           Object.keys(dict).map(key => <option key={key} value={key}>{dict[key].lang}</option>)
         }
