18bf4e2b6760a37645e2d9f52b4d81da5ac4725a
Chenqi <1002652154@qq.com>
2026-01-13 22:21:36 +0100

    add error boundary showing error stack

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
 import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallbackRender={({ error }) => <div>{error.stack}</div>}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
